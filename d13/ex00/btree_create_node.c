073f00246011e5c2e89d4c130075e07a1a1b341f
Donald Stolz <dstolz@luc.edu>
2016-07-30 14:27:45 -0700

    Update btree_create_node.c

--- d13/ex00/btree_create_node.c
+++ d13/ex00/btree_create_node.c
@@ -10,7 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
- t_btree *btree_create_node(void *item)
- {
- 	
- }
+#include "ft_btree.h"
+
+t_btree		*btree_create_node(void *item)
+{
+	t_btree *node;
+
+	node = malloc(sizeof(*node));
+	if (node)
+	{
+		node->item = item;
+		node->left = 0;
+		node->right = 0;
+		return (node);
+	}
+	return (0);
+}
